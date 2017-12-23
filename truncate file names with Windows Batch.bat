a67e2c1070880ac39b88682d46a21a11685a5096
Abdessamed Kohli - Certified <abdessamed.contact.official+Github@gmail.com>
2017-12-23 03:20:38 +0100

    Create truncate file names with Windows Batch.bat

---
+++ truncate file names with Windows Batch.bat
@@ -0,0 +1,14 @@
+
+
+Rename/truncate file names with Windows Batch January 26, 2013
+http://minhou-tech.webtegration.com/blog/2013/01/26/renametruncate-file-names-with-windows-batch/
+
+@echo off
+setlocal enabledelayedexpansion
+set deletestring=access_
+for /f "delims==" %%F in ('dir /b ^| find "%deletestring%"') do (
+set oldfilename=%%F
+set newfilename=!oldfilename:%deletestring%=!
+Ren "!oldfilename!" "!newfilename!"
+)
+ren 20*.log ex*.log
